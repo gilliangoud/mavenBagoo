d734606874e2ce23e3719541af758dbc45fc6e77
Jules <jules.ishimwe@hva.nl>
2017-12-08 15:45:03 +0100

    rapporten

--- src/main/java/com/fortw/bagoo/RapportenController.java
+++ src/main/java/com/fortw/bagoo/RapportenController.java
@@ -5,9 +5,17 @@
  */
 package com.fortw.bagoo;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +23,24 @@ import javafx.fxml.Initializable;
  * @author jules
  */
 public class RapportenController implements Initializable {
-
+@FXML
+    private AnchorPane schermRapporten;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+    @FXML
+    private void handleAnnuleerAction(ActionEvent event) throws IOException {
+              FXMLLoader fxmlLoader = new FXMLLoader (getClass().getResource("HoofdSchermManagement.fxml"));
+    Parent root1 = (Parent) fxmlLoader.load();
+    Stage stageVolgende = new Stage();
+    stageVolgende.setScene(new Scene (root1));
+    Stage stageHuidige = (Stage) schermRapporten.getScene().getWindow();
+    stageHuidige.close();
+    stageVolgende.show();
+    }
     
 }
